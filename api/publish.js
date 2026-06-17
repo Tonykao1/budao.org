27093f951bfb3202ab65df3c351dcd2147fe7e23
Tonykao1 <69593498+Tonykao1@users.noreply.github.com>
2026-06-17 23:57:55 +0800

    Update publish.js

--- api/publish.js
+++ api/publish.js
@@ -1,8 +1,17 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
 
-    res.status(200).json({
-        success:true,
-        message:"佳音启程"
+  if (req.method !== 'POST') {
+    return res.status(200).json({
+      success: true,
+      message: '佳音启程'
     });
+  }
+
+  const route = req.body;
+
+  return res.status(200).json({
+    success: true,
+    route
+  });
 
 }
