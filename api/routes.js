aab91800ca446070f0cdc3487a9cf880ed3520fc
Tonykao1 <69593498+Tonykao1@users.noreply.github.com>
2026-08-07 01:37:49 +0800

    Disable route API caching

--- api/routes.js
+++ api/routes.js
@@ -21,9 +21,10 @@ module.exports = async function handler(request, response) {
   try {
     const routes = await readRoutes();
 
-    response.setHeader("Cache-Control", "s-maxage=0, stale-while-revalidate=30");
+    response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
     sendJson(response, 200, fixedRoutesOnly(routes));
   } catch (error) {
+    response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
     sendJson(response, 200, []);
   }
 };
